15e5d29f1500ac197d8e50c7b846f57ef4f1f785
lucasr <lucasregoaraujo@gmail.com>
2025-02-10 16:31:15 -0300

    Subindo o teste

---
+++ teste/Program.cs
@@ -0,0 +1,127 @@
+﻿namespace teste
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            var grafo = new Dictionary<string, string>();
+
+            var idsArmazem = new HashSet<int>() { 2, 4 };
+
+            object[][] grid = 
+            [
+                [1, 1, "estrada", 2, 2],
+
+                [1, "estrada", "estrada", "estrada", 2],
+
+                ["estrada", "estrada", "estrada", "estrada", ""],
+
+                [3, "estrada", "", "estrada", 4],
+
+                [3, 3, "estrada", 4, 4]
+            ];
+
+            int linhas = grid.Length;
+            int colunas = grid[0].Length;
+
+            for (int i = 0; i < linhas; i++) 
+            {    
+                for(int j = 0; j < colunas; j++)
+                {                   
+                    PercorrerGridEAdicionaElementos(i, j);
+                }
+            }
+
+            foreach ((string key, string value) in grafo)
+            {
+                Console.WriteLine($"{key} -- {value}");
+            }
+
+
+            void PercorrerGridEAdicionaElementos(int linha, int coluna)
+            {
+                var noParaVisitar = new List<int>() { linha, coluna };
+                var visitados = new List<int[]>();                
+
+                var indicesOrtogonais = new List<Tuple<int, int>>()
+                {
+                    Tuple.Create(-1, 0),
+                    Tuple.Create(1, 0),
+                    Tuple.Create(0, -1),
+                    Tuple.Create(0, 1),
+                };
+
+                for (int i = 0; i < noParaVisitar.Count; i++)
+                {
+                   
+                    int x, y;
+                    x = noParaVisitar[0]; 
+                    y = noParaVisitar[1]; 
+
+                    var elementoAtual = grid[x][y];
+                    noParaVisitar.Clear();
+
+                    foreach ((int dx, int dy) in indicesOrtogonais)
+                    {
+                        int nx = x + dx;
+                        int ny = y + dy;
+                        if ( (0 <= nx && nx < linhas)
+                          && (0 <= ny && ny < colunas)
+                          && (!visitados.Contains([nx, ny])))
+                        {
+                            
+                            if (grid[nx][ny].ToString() == "estrada") 
+                            {
+                                int larguraEstrada = 1;
+                                if(dx != 0)
+                                {
+                                    larguraEstrada = grid.Select(row => row[y].ToString() == "estrada").Count();
+                                }
+                                else
+                                {
+                                    larguraEstrada = grid[x].Count(e => e.ToString() == "estrada");
+                                }
+
+                                bool isIntersection = indicesOrtogonais.Any(dir =>
+                                {
+                                    int ix = nx + dir.Item1;
+                                    int iy = ny + dir.Item2;
+                                    return (0 <= ix && ix < linhas && 0 <= iy && iy < colunas && grid[ix][iy].ToString() == "estrada");
+                                });
+
+                                if (isIntersection)
+                                {
+                                    grafo[$"({nx},{ny})"] = $"Interseção de estrada";
+                                }
+                                else
+                                {
+                                    grafo[$"({nx},{ny})"] = $"Estrada. Peso={larguraEstrada}";
+                                }
+
+                                noParaVisitar.AddRange([nx, ny]);
+                                visitados.Add([nx, ny]);
+                            }
+                            else if (string.IsNullOrEmpty(grid[nx][ny].ToString()))
+                            {
+                                grafo[$"({nx},{ny})"] = $"Terminacao de estrada";
+                            }
+                            else if(grid[nx][ny].GetType() == typeof(int))
+                            {
+                                int armazem = idsArmazem.FirstOrDefault(e => e == (int) grid[nx][ny]);
+                                if(armazem != default)
+                                {
+                                    grafo[$"({nx},{ny})"] = $"Armazem: {grid[nx][ny]}";
+                                }
+                                else
+                                {
+                                    grafo[$"({nx},{ny})"] = $"Predio: {grid[nx][ny]}";
+                                }                                
+                            }
+                        }                        
+                    }
+                }
+            }                             
+        }       
+    }
+}
---
+++ teste/readme.md
@@ -0,0 +1,67 @@
+﻿# Transformação de Grade em Grafo
+
+Este projeto em C# transforma uma grade de espaços em um grafo, onde os vértices representam conexões entre estradas e os nós representam prédios, interseções de estradas, mudanças na estrada e terminações de estradas.
+
+## Estrutura do Projeto
+
+- `Program.cs`: Contém o código principal para transformar a grade em um grafo e imprimir as conexões.
+
+## Pré-requisitos
+
+- .NET SDK 6.0 ou superior
+
+## Como Rodar o Programa
+
+1. Clone o repositório:
+   ```
+   git clone https://github.com/seu-usuario/seu-repositorio.git
+   cd seu-repositorio
+   ```
+2. Compile o Projeto
+	```
+    dotnet build 
+    ```
+3. Execute o Programa
+	```
+	dotnetrun
+    ```
+## Exemplos de Uso
+
+O código transforma a seguinte grade:
+```
+[
+    [1, 1, "estrada", 2, 2],
+    [1, "estrada", "estrada", "estrada", 2],
+    ["estrada", "estrada", "estrada", "estrada", ""],
+    [3, "estrada", "", "estrada", 4],
+    [3, 3, "estrada", 4, 4]
+]
+```
+
+Em um grafo com as seguintes conexões
+
+```
+(0,0) -- Predio: 1
+(0,2) -- Interseçao de estrada
+(2,1) -- Interseçao de estrada
+(1,2) -- Interseçao de estrada
+(3,1) -- Interseçao de estrada
+(2,0) -- Interseçao de estrada
+(2,2) -- Interseçao de estrada
+(0,3) -- Armazem: 2
+(1,3) -- Interseçao de estrada
+(0,4) -- Armazem: 2
+(2,3) -- Interseçao de estrada
+(1,4) -- Armazem: 2
+(3,3) -- Interseçao de estrada
+(2,4) -- Terminacao de estrada
+(3,0) -- Predio: 3
+(3,2) -- Terminacao de estrada
+(3,4) -- Armazem: 4
+(4,0) -- Predio: 3
+(4,1) -- Predio: 3
+(4,2) -- Estrada. Peso=1
+(4,3) -- Armazem: 4
+(4,4) -- Armazem: 4
+
+```
